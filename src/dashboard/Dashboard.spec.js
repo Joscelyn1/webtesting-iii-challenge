863213cc541369e822f49aba15489daa30231ba5
Joscelyn <JoscelynRose@gmail.com>
2019-08-19 18:49:59 -0400

    WIP: dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+import { render, fireEvent, getByTestId } from '@testing-library/react';
+import Dashboard from '../dashboard/Dashboard.js';
+import Controls from './Controls';
+
+describe('<Dashboard />', () => {
+  // 2. write this test
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Controls />); // generates a DOM tree
+
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
