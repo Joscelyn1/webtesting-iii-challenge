92b017ad990fcf4540d271df3c0c9bd1ecb5467d
Joscelyn <JoscelynRose@gmail.com>
2019-08-19 18:52:03 -0400

    WIP: display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+import { render, fireEvent, getByTestId } from '@testing-library/react';
+import Display from './Display';
+
+describe('<Display />', () => {
+  // 2. write this test
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Display />); // generates a DOM tree
+
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
